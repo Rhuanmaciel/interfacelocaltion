e6bc5e93ee0cabbe42bc29d3dbf2ec08fa42a950
Rhuanmaciel <rhuanc01@gmail.com>
2023-05-13 23:43:54 -0300

    Initial commit

--- public/app.js
+++ public/app.js
@@ -3,6 +3,14 @@ let marker;
 let selectedUser;
 
 const socket = io.connect('http://your-websocket-server.com');
+const path = require('path');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+    res.sendFile('index.html');
+});
+
 
 // Iniciar o mapa
 function initMap() {
